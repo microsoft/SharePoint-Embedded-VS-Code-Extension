391f3567ccd52151b3d425893fde86f9910cd85a
Alex Jordache <aljordac@microsoft.com>
2024-04-04 12:01:15 -0700

    add session id

--- src/services/TelemetryProvider.ts
+++ src/services/TelemetryProvider.ts
@@ -8,9 +8,13 @@ import { TelemetryEvent, TelemetryErrorEvent } from '../models/telemetry/telemet
 class TelemetryProvider {
     public static readonly instance: TelemetryProvider = new TelemetryProvider(); 
     private reporter: TelemetryReporter;
+    private _sessionId;
+    private _sessionStartTime;
 
     public constructor() {
         this.reporter = new TelemetryReporter(telemetryKey);
+        this._sessionId = uuidv4();
+        this._sessionStartTime = Date.now();
     }
 
     public send(ev: TelemetryEvent): void {
@@ -37,12 +41,16 @@ class TelemetryProvider {
 
     public sendTelemetryEvent(ev: TelemetryEvent): void {
         if (this.reporter) {
+            ev.addProperty("sessionId", this._sessionId);      
+            ev.addProperty("sessionDurationMinutes", ((Date.now() - this._sessionStartTime) / 60000).toString());
             this.reporter.sendTelemetryEvent(ev.name, ev.properties);
         }
     }
 
     public sendTelemetryErrorEvent(ev: TelemetryErrorEvent): void {
         if (this.reporter) {
+            ev.addProperty("sessionId", this._sessionId);      
+            ev.addProperty("sessionDurationMinutes", ((Date.now() - this._sessionStartTime) / 60000).toString());
             this.reporter.sendTelemetryErrorEvent(ev.name, ev.properties);
         }
     }
