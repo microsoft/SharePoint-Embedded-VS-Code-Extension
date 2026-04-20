0d17f722140ddcaf4c25e501060ad9ad18208eda
Alex Jordache <aljordac@microsoft.com>
2026-04-20 14:51:43 -0700

    ready

--- src/commands/ContainerType/BrowseGraphExplorer.ts
+++ src/commands/ContainerType/BrowseGraphExplorer.ts
@@ -7,7 +7,7 @@ import * as vscode from 'vscode';
 import { ContainerTypeTreeItem } from '../../views/treeview/development/ContainerTypeTreeItem';
 import { Command } from '../Command';
 
-const GRAPH_EXPLORER_URL = 'https://developer.microsoft.com/en-us/graph/graph-explorer?devx-api=https://devxapi-func-prod-eastus.azurewebsites.net&org=marcwindle&branchName=mawin/add-sharepoint-embedded-sample-queries';
+const GRAPH_EXPLORER_URL = 'https://developer.microsoft.com/graph/graph-explorer';
 
 // Static class that opens Graph Explorer to browse SharePoint Embedded APIs
 export class BrowseGraphExplorer extends Command {
